5c86b42c8a175b8fa2135ea4a7709f9c5704532a
DanhNguyen\Danh Nguyen <chidanh0502@gmail.com>
2023-12-16 21:45:10 +0700

    - add new entity

---
+++ MusicServerAPI/Entity/Album.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MusicServerAPI.Entity
+{
+    public class Album
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? ImageURL { get; set; }
+
+        [Required]
+        public Artist? Artist { get; set; }
+
+        [AllowNull]
+        [DataType(DataType.DateTime)]
+        public DateTime? ReleaseDate { get; set; }
+
+        public virtual ICollection<Song>? Songs { get; set; }
+    }
+}
